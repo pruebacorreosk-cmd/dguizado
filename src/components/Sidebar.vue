a845cf5bbe8cef4789e7009d09b64f69f5704e0c
Daniel <prueba.correo.sk@gmail.com>
2026-02-02 09:16:00 -0500

    Instala vee validate, crea modulos

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,9 +1,11 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div class="sidebar">
     <h2>Barra Lateral</h2>
     <ul>
       <li><router-link to="/contador">Contador</router-link></li>
       <li><router-link to="/lista-de-tareas">Lista de Tareas</router-link></li>
+      <li><router-link to="/registrar">Registrar</router-link></li>
     </ul>
   </div>
 </template>
---
+++ src/modules/registro/views/RegistroView.vue
@@ -0,0 +1,35 @@
+<!-- eslint-disable vue/valid-template-root -->
+<template>
+  <div>
+    <h2>Formulario de registro</h2>
+    <Form :validation-schema="schema" @submit="onSubmit">
+      <div class="form">
+        <label for="nombre">Nombre</label>
+        <Field type="text" name="nombre" id="nombre" placeholder="ingrese su nombre" />
+        <ErrorMessage name="nombre"></ErrorMessage>
+      </div>
+      <div class="form">
+        <label for="correo">Correo</label>
+        <Field type="email" name="email" id="correo" placeholder="ingrese su email" />
+        <ErrorMessage name="email"></ErrorMessage>
+      </div>
+      <div class="form">
+        <button type="submit">Registrar</button>
+      </div>
+    </Form>
+  </div>
+</template>
+
+<script setup>
+import { Form, Field, ErrorMessage } from "vee-validate";
+import { schema } from "../schemas/validationSchema";
+
+const onSubmit = () => {
+  console.log("Se a enviado el formulario");
+};
+</script>
+<style scope>
+.form {
+  margin-bottom: 10px;
+}
+</style>
